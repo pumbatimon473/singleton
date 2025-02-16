a9e00f7c7d1b164bb18153defc6ab2dd7ce1272e
Anurag Kumar <pumbatimon473@gmail.com>
2025-02-16 17:17:18 +0530

    Singleton v5: Leveraging class loading mechanism

--- src/main/java/com/example/singleton/Singleton.java
+++ src/main/java/com/example/singleton/Singleton.java
@@ -4,33 +4,31 @@ package com.example.singleton;
  * Design Pattern: Singleton
  * - Type: Creational
  * 
- * Implementation: v4
- * - Using synchronized block - Double Checked Lock (DCL)
+ * Implementation: v5
+ * - Taking leverage of class loading mechanism:
+ *  -- JVM guarantees that classes are loaded in a thread safe manner (i.e., they are loaded only once)
+ *  -- static inner classes are lazily loaded
  * 
  * Pros:
  * - Lazy initialization - The object is initialized on demand
  * - Thread Safe
- * - Faster than synchronized method
+ * - Does not need explicit synchronization
  * 
- * Cons:
- * - A bit complex
  */
 class Singleton {
-    // step 1: declare a private static var of type Singleton
-    private static volatile Singleton instance;
-
-    // step 2: make the CTOR private
+    // step 1: make the CTOR private
     private Singleton() {
         // object initialization logic
     }
 
+    // step 2: make a static inner class
+    private static class SingletonHolder {
+        // initialize a static var of type Singleton
+        private static final Singleton INSTANCE = new Singleton();
+    }
+
     // step 3: provide a global access interface
     public static Singleton getInstance() {
-        if (instance == null)  // 1st check
-            synchronized (Singleton.class) {  // acquiring lock only when instance is null
-                if (instance == null)  // 2nd check
-                    instance = new Singleton();
-            }
-        return instance;
+        return SingletonHolder.INSTANCE;
     }
 }
