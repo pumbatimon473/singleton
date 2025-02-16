2d4d3c8b9d9d2cdbdda820cd6e3f3f2654bb07a2
Anurag Kumar <pumbatimon473@gmail.com>
2025-02-16 17:04:34 +0530

    Singleton v4: Double Checked Lock (DCL)

--- src/main/java/com/example/singleton/Singleton.java
+++ src/main/java/com/example/singleton/Singleton.java
@@ -4,20 +4,20 @@ package com.example.singleton;
  * Design Pattern: Singleton
  * - Type: Creational
  * 
- * Implementation: v3
- * - Using synchronized method
+ * Implementation: v4
+ * - Using synchronized block - Double Checked Lock (DCL)
  * 
  * Pros:
  * - Lazy initialization - The object is initialized on demand
  * - Thread Safe
+ * - Faster than synchronized method
  * 
  * Cons:
- * - Allows only 1 thread to access the method: getInstance()
- *  -- Slow
+ * - A bit complex
  */
 class Singleton {
     // step 1: declare a private static var of type Singleton
-    private static Singleton instance;
+    private static volatile Singleton instance;
 
     // step 2: make the CTOR private
     private Singleton() {
@@ -25,9 +25,12 @@ class Singleton {
     }
 
     // step 3: provide a global access interface
-    public static synchronized Singleton getInstance() {
-        if (instance == null)
-            instance = new Singleton();
+    public static Singleton getInstance() {
+        if (instance == null)  // 1st check
+            synchronized (Singleton.class) {  // acquiring lock only when instance is null
+                if (instance == null)  // 2nd check
+                    instance = new Singleton();
+            }
         return instance;
     }
 }
