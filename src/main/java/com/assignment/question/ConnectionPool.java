21df0abad73fba53a37ff05952ae13dd0015cf72
Anurag Kumar <pumbatimon473@gmail.com>
2025-02-16 21:01:54 +0530

    Singleton Assignment 3 - Problem Statement

---
+++ src/main/java/com/assignment/question/ConnectionPool.java
@@ -0,0 +1,16 @@
+package com.assignment.question;
+
+
+public interface ConnectionPool {
+
+    void initializePool();
+
+    DatabaseConnection getConnection();
+
+    void releaseConnection(DatabaseConnection connection);
+
+    int getAvailableConnectionsCount();
+
+    int getTotalConnectionsCount();
+
+}
---
+++ src/main/java/com/assignment/question/DatabaseConnection.java
@@ -0,0 +1,4 @@
+package com.assignment.question;
+
+public class DatabaseConnection {
+}
